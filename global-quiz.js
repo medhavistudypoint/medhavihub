f2416d1b0aedd6237d6a0853744bb074221b48cb
medhavistudypoint <medhavistudypoint@gmail.com>
2026-08-06 00:23:49 +0530

    Update global-quiz.js

--- global-quiz.js
+++ global-quiz.js
@@ -1,4 +1,4 @@
-Const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz1Rg1CjzNnB9bqXSyF552kFhG87JFk8D_NlRN07gtE7Yft70mQiZfzyeH2PevcFyo_/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz1Rg1CjzNnB9bqXSyF552kFhG87JFk8D_NlRN07gtE7Yft70mQiZfzyeH2PevcFyo_/exec";
 
 let activeIndex = 0;
 // HTML के countdownTimer डिब्बे से समय (उदा. 55:00) पढ़कर टाइमर सेट करें
